97213675c65b2cdaa56f1ea9b85d107ae8ada600
youssef <jsf.ch6@gmail.com>
2024-10-01 14:23:56 +0200

    feat: Add product details fetching via injectEndpoints in ApiSlice

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,33 +1,33 @@
-import { useEffect , useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row , Col , Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 // import products from "../products";
-import axios from "axios";
+import {useGetProductByIdQuery} from "../slices/productsApiSlice";
+
 
 const ProductScreen = () => {
-  const [product , setProduct] = useState({});
-    const { id: productId } = useParams();
-    // const product = products.find(product => product?._id === productId);
-    useEffect(()=>{
-      const fetchProduct = async () => {
-        const {data} = await axios.get(`/api/products/${productId}`);
-        setProduct(data);
-      }
-      fetchProduct();
-    },[productId])
+    const { id :productId} = useParams();
+    console.log("productId",productId);
+    const { data:product, isLoading, error } = useGetProductByIdQuery(productId);
+
 
   return (
+
     <>
     <Link className="btn btn-light my-3" to='/'> Go Back </Link>
+   {isLoading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h1>{error?.data?.message || error.error}</h1>
+      ) : (
     <Row>
-        <Col md={5} ><Image src={product?.image} alt={product.name} fluid /></Col>
+        <Col md={5} ><Image src={product?.image} alt={product?.name} fluid /></Col>
         <Col md={4} >
         <ListGroup variant='flush'>
-        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-        <ListGroup.Item>Rating: <Rating value={product.rating} text={`${product.numReviews} reviews`} /></ListGroup.Item>
-        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+        <ListGroup.Item>Price: ${product?.price}</ListGroup.Item>
+        <ListGroup.Item>Rating: <Rating value={product?.rating} text={`${product?.numReviews} reviews`} /></ListGroup.Item>
+        <ListGroup.Item>Description: {product?.description}</ListGroup.Item>
         </ListGroup>
          </Col>
 
@@ -38,26 +38,26 @@ const ProductScreen = () => {
             <ListGroup.Item>
                 <Row>
                     <Col>Price:</Col>
-                    <Col><strong>${product.price}</strong></Col>
+                    <Col><strong>${product?.price}</strong></Col>
                 </Row>
             </ListGroup.Item>
 
             <ListGroup.Item>
                 <Row>
                     <Col>Status:</Col>
-                    <Col><strong>${product.countInStock > 0 ? "In Stock" : "Out of Stock"}</strong></Col>
+                    <Col><strong>${product?.countInStock > 0 ? "In Stock" : "Out of Stock"}</strong></Col>
                 </Row>
             </ListGroup.Item>
 
             <ListGroup.Item>
-                <Button className="btn-block"  type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
+                <Button className="btn-block"  type='button' disabled={product?.countInStock === 0}>Add to Cart</Button>
             </ListGroup.Item>
             
           </ListGroup>
          </Card>
          </Col>
 
-    </Row>
+    </Row>)}
     </>
   )
 }
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,7 +4,8 @@ import { apiSlice } from './apiSlice';
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({ query: () => ({ url: PRODUCTS_URL }), keepUnusedDataFor: 5, }),
+    getProductById: builder.query({ query: (id) => ({ url: `${PRODUCTS_URL}/${id}` }), keepUnusedDataFor: 5, }),
   }),
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery , useGetProductByIdQuery } = productsApiSlice;
