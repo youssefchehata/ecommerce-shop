825b38271be2d93baefd6762e68e374d0a9f4ebb
youssef <jsf.ch6@gmail.com>
2024-10-01 19:28:04 +0200

    Add addToCart action in cartSlice and addToCartHandler in ProductScreen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,18 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useParams , useNavigate} from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Row , Col , Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Row , Col , Image, ListGroup, Card, Button, Form } from "react-bootstrap";
 import Rating from "../components/Rating";
 // import products from "../products";
 import {useGetProductByIdQuery} from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
+import { useDispatch } from "react-redux";
+
 
 
 const ProductScreen = () => {
     const { id :productId} = useParams();
-    console.log("productId",productId);
-    const { data:product, isLoading, error } = useGetProductByIdQuery(productId);
-
+     const navigate = useNavigate();
+     const dispatch = useDispatch();
+     const [qty ,setQty] = useState(1);
+     const { data:product, isLoading, error } = useGetProductByIdQuery(productId);
+     const addToCartHandler =()=>{
+      dispatch(addToCart({...product, qty}));
+      navigate(`/cart`);
+  }
 
   return (
 
@@ -51,11 +60,30 @@ const ProductScreen = () => {
                     <Col><strong>${product?.countInStock > 0 ? "In Stock" : "Out of Stock"}</strong></Col>
                 </Row>
             </ListGroup.Item>
-
+          
+          {product?.countInStock > 0 && (
             <ListGroup.Item>
-                <Button className="btn-block"  type='button' disabled={product?.countInStock === 0}>Add to Cart</Button>
+                <Row>
+                    <Col>Qty:</Col>
+                    <Col>
+                    <Form.Control
+                    as='select'
+                    value={qty}
+                    onChange={(e) => setQty(Number(e.target.value))}
+                  >
+                    {[...Array(product?.countInStock).keys()].map((i) => (
+                      <option key={i} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </Form.Control>
+                    </Col>
+                </Row>
+           </ListGroup.Item>
+          )}
+            <ListGroup.Item>
+                <Button onClick={addToCartHandler} className="btn-block"  type='button' disabled={product?.countInStock === 0}>Add to Cart</Button>
             </ListGroup.Item>
-            
           </ListGroup>
          </Card>
          </Col>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []};
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] };
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      // check if item already exist in cart
+      const existItem = state.cartItems.find( (cartItem) => cartItem._id === item._id );
+      if (existItem) {
+        state.cartItems = state.cartItems.map((el) => el._id === item._id ? item : el );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      // calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      // calculate shipping price if order is over $100 then free else $10 shipping
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      //calculate tax price 15% tax
+      state.taxPrice = addDecimals(Number(state.itemsPrice * 0.15).toFixed(2));
+      //calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
 });
-
+ export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
