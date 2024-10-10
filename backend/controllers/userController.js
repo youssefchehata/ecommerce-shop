38a19ce1258b0aab6c6c4d890f165e2d52ee6aee
youssef <jsf.ch6@gmail.com>
2024-10-10 19:35:39 +0200

    feat: implement password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,8 +5,21 @@ import User from "../models/userModel.js";
 //@route  POST /api/users/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-    console.log(req.body);
-res.send("auth user")
+    const {email , password}= req.body;
+    const user = await User.findOne({email});
+    if(user && await user.matchPassword(password)){
+        res.json({
+            _id:user._id,
+            email:user.email,
+            name:user.name,
+            isAdmin:user.isAdmin 
+                })
+    }else{
+        res.status(401);
+        throw new Error('Invalid email or password')
+    }
+
+
 });
 
 //@desc   Register user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 
@@ -23,7 +24,10 @@ const userSchema = new Schema({
       default: false,
     },
   }, { timestamps: true });
-  
+
+  userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+    };
   const User = mongoose.model('User', userSchema);
   
   export default User;
