3c8945a59f68fb987f1a9a109f01058d1ea1c80c
youssef <jsf.ch6@gmail.com>
2024-10-10 16:48:42 +0200

    feat: prepare UserRoutes and controller

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,18 @@ import connectDB from './config/db.js';
 connectDB();
 // import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5000;
 const app = express();
 
-
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
