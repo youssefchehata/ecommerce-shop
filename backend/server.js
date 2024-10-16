1b417ca78dc9d271eb21719ba4ea8ab34abd04b1
youssef <jsf.ch6@gmail.com>
2024-10-16 17:59:17 +0200

    feat: add order methods, controller, and routes in server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ connectDB();
 // import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
