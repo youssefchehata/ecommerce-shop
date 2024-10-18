caba2e6d360357b93dcf574222f2391e7e67fe76
youssef <jsf.ch6@gmail.com>
2024-10-18 12:36:11 +0200

    feat: profile screen frontEnd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import ProfileScreen from './screens/ProfileScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -40,6 +41,9 @@ const router = createBrowserRouter(
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
         <Route path='/order/:id' element={<OrderScreen />} />
+        <Route path='/profile' element={<ProfileScreen />} />
+
+        
       </Route>
     </Route>
   )
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -6,8 +6,9 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     login: builder.mutation({ query: (data) => ({ url: `${USERS_URL}/login`, method: 'POST', body: data }) }),
     logout: builder.mutation({ query: () => ({ url: `${USERS_URL}/logout`, method: 'POST', }), }),
     register: builder.mutation({ query: (data) => ({ url: `${USERS_URL}`, method: 'POST', body: data }) }),
+    profile: builder.mutation({ query: (data) => ({ url: `${USERS_URL}/profile`, method: 'PUT', body: data, }), }),
 
   }),
 });
 
-export const { useLoginMutation , useLogoutMutation , useRegisterMutation } = usersApiSlice;
+export const { useLoginMutation , useLogoutMutation , useRegisterMutation ,useProfileMutation} = usersApiSlice;
