f15badab4b4d559ce31ede87365ef242883c02da
youssef <jsf.ch6@gmail.com>
2024-10-21 18:16:04 +0200

    feat: add image upload functionality in product edit (server-side)

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import express from 'express';
+
+import path from 'path';import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
@@ -9,6 +10,7 @@ connectDB();
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -24,10 +26,29 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -3,7 +3,7 @@ import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getProducts: builder.query({ query: () => ({ url: PRODUCTS_URL }), keepUnusedDataFor: 5, }),
+    getProducts: builder.query({ query: () => ({ url: PRODUCTS_URL }), keepUnusedDataFor: 5,providesTags:['Products'] }),
     getProductById: builder.query({ query: (id) => ({ url: `${PRODUCTS_URL}/${id}` }), keepUnusedDataFor: 5, }),
     createProduct: builder.mutation({ query: () => ({ url: `${PRODUCTS_URL}`, method: 'POST', }), invalidatesTags: ['Product'], }),
     deleteProduct: builder.mutation({ query: (productId) => ({ url: `${PRODUCTS_URL}/${productId}`, method: 'DELETE', }), providesTags: ['Product'], }),
